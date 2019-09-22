b2bbce6400e536a65a99de82ee5a7bfd21c0d6d9
Juseluscas <jos3.lucasjlp@gmail.com>
2019-09-22 19:06:13 -0300

    estruturar vitoria do game

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ let coluna7 = document.querySelector('#coluna7')
 let coluna8 = document.querySelector('#coluna8')
 let coluna9 = document.querySelector('#coluna9')
 
+
+
 var vezJogador = getRandomInt(1,2);
 
 function getRandomInt(min, max) {
@@ -65,56 +67,73 @@ function j2O(){
 function X1(){
     coluna1.innerHTML = ("X");
     coluna1.style.color = "red";
-    var coluna
+    var cx1 = 1;
+    return cx1;
     j2O();
 }
 
 function X2(){
     coluna2.innerHTML = ("X");
     coluna2.style.color = "red";
+    var cx2 = 1;
     j2O();
+    
 }
 
 function X3(){
     coluna3.innerHTML = ("X");
     coluna3.style.color = "red";
+    var cx3 = 1;
     j2O();
+    
 }
 
 function X4(){
     coluna4.innerHTML = ("X");
     coluna4.style.color = "red";
+    var cx4 = 1;
     j2O();
+    
 }
 
 function X5(){
     coluna5.innerHTML = ("X");
     coluna5.style.color = "red";
+    var cx5 = 1;
     j2O();
+    
 }
 
 function X6(){
     coluna6.innerHTML = ("X");
     coluna6.style.color = "red";
+    var cx6 = 1;
     j2O();
+    
 }
 
 function X7(){
     coluna7.innerHTML = ("X");
     coluna7.style.color = "red";
+    var cx7 = 1;
     j2O();
+    
 }
 
 function X8(){
     coluna8.innerHTML = ("X");
     coluna8.style.color = "red";
+    var cx8 = 1;
     j2O();
+    
 }
 
 function X9(){
     coluna9.innerHTML = ("X");
     coluna9.style.color = "red";
+    var cx9 = 1;
     j2O();
+    
 }
 
 function O1(){
@@ -170,5 +189,16 @@ function O9(){
     coluna9.style.color = "blue";
     j1X();
 }    
+
+
+function ganhou(){
+    if((cx1 + cx2 + cx3) == 3){ 
+        let textjogador = ("VOCÊ GANHOU!!!");
+        textgamer.innerHTML = textjogador;
+        alert("Parabens Você Ganhou");  
+        }
+    }
+
+
 
 botao.onclick = Selecao;
